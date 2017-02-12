0c864504f6cc2f38f6e69f6eae385c138766cc96
Baggers <chris.bagley@gmail.com>
2017-02-12 20:02:02 +0100

    clean up existing line-segment3 code

--- regions/line-segment3.lisp
+++ regions/line-segment3.lisp
@@ -1,35 +1,58 @@
 (in-package :rtg-math.regions.line-segment3)
 
+;; A line segment in ℝ3
+
 (defstruct line-segment3
   (end-point0 (v! 0 0 0) :type vec3)
   (offset (v! 0 1 0) :type vec3))
 
-(defun line-segment3-end-point1 (line-seg3)
+(defn line-segment3-end-point1 ((line-seg3 line-segment3)) vec3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
   (v3:+ (line-segment3-end-point0 line-seg3)
         (line-segment3-offset line-seg3)))
 
-(defun make (end-point0 end-point1)
-  (make-line-segment3 :end-point0 end-point0
-                      :offset (v:- end-point1 end-point0)))
+(defn-inline end-point0 ((line-seg3 line-segment3)) vec3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
+  (line-segment3-end-point0 line-seg3))
 
-(defun make-from-point-offset (point-v3 offset-v3)
+(defn-inline end-point1 ((line-seg3 line-segment3)) vec3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
+  (line-segment3-end-point1 line-seg3))
+
+(defn direction ((line-seg3 line-segment3)) vec3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
+  (line-segment3-offset line-seg3))
+
+(defn make ((end-point0 vec3) (end-point1 vec3)) line-segment3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
+  (make-line-segment3 :end-point0 end-point0
+                      :offset (v3:- end-point1 end-point0)))
+
+(defn make-from-point-offset ((point-v3 vec3) (offset-v3 vec3)) line-segment3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
   (make-line-segment3 :end-point0 point-v3
                       :offset offset-v3))
 
-(defun transform-m3 (matrix3 line3)
-  (make-line-segment3
-   :end-point0 (m3:*v matrix3 (line-segment3-end-point0 line3))
-   :offset (m3:*v matrix3 (line-segment3-offset line3))))
+;;----------------------------------------------------------------
 
-(defun transform-q (quaternion line3)
-  (transform-m3 (q:to-mat3 quaternion)
-                line3))
+(defn transform-m3 ((matrix3 mat3) (line-seg3 line-segment3)) line-segment3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
+  (make (m3:*v matrix3 (end-point0 line-seg3))
+        (m3:*v matrix3 (direction line-seg3))))
 
-(defun direction (line-seg3)
-  (line-segment3-offset line-seg3))
+(defn transform-q ((quat quaternion) (line-seg3 line-segment3)) line-segment3
+  (declare (optimize (speed 3) (safety 1) (debug 1)))
+  (make (q:rotate (end-point0 line-seg3) quat)
+        (q:rotate (direction line-seg3) quat)))
 
-(defun length (line3)
-  (v3:length (line-segment3-offset line3)))
+;;----------------------------------------------------------------
 
-(defun length-squared (line3)
-  (v3:length-squared (line-segment3-offset line3)))
+(defn length-squared ((line-seg3 line-segment3))
+    (single-float 0f0 #.most-positive-single-float)
+  (v3:length-squared (line-segment3-offset line-seg3)))
+
+(defn length ((line-seg3 line-segment3))
+    (single-float 0f0 #.most-positive-single-float)
+  (v3:length (line-segment3-offset line-seg3)))
+
+;;----------------------------------------------------------------
