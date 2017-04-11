dd656e7760720a29c6a1856b965c8cdbc39d1f08
baggers <baggers@cave>
2017-04-11 21:04:10 +0200

    Fix orthographic projection matrix function

--- projection/camera.lisp
+++ projection/camera.lisp
@@ -27,7 +27,7 @@
         (near near )
         (far far ))
     (m4:make
-     (/ 2 (- right left)) 0.0 0.0 (- (/ (+ right left) (- left right)))
-     0.0 (/ 2 (- top bottom)) 0.0 (- (/ (+ top bottom) (- bottom top)))
-     0.0 0.0 (- (/ (- far near))) (- (/ (+ far near) (- far near)))
-     0.0 0.0 0.0 1.0)))
+     (/ 2f0 (- right left)) 0f0                    0f0                   (- (/ (+ right left) (- right left)))
+     0f0                    (/ 2f0 (- top bottom)) 0f0                   (- (/ (+ top bottom) (- top bottom)))
+     0f0                    0f0                    (/ -2f0 (- far near)) (- (/ (+ far near) (- far near)))
+     0f0                    0f0                    0f0                   1f0)))
