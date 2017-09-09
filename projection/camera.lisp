0cd03e4bf5fa9536b0c2af2558fc7568f738e236
cbaggers <chris.bagley@gmail.com>
2017-09-09 13:40:04 +0200

    Gave up on old perspective impl and used one checked against GLM

--- projection/camera.lisp
+++ projection/camera.lisp
@@ -2,37 +2,26 @@
 
 ;;------------------------------------------------------------
 
-;; https://unspecified.wordpress.com/2012/06/21/calculating-the-gluperspective-matrix-and-other-opengl-matrix-maths/
-;; https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/gluPerspective.xml
+;; Checked against GLM's perspectiveRH
 (defn perspective-radian-fov ((width single-float) (height single-float)
                               (near single-float) (far single-float)
                               (fov single-float))
     mat4
+
   ;; note: is right handed
   (declare (optimize (speed 3) (safety 1) (debug 1)))
-  (let* ((d (/ 1f0 (tan (* fov 0.5f0))))
-         (recip (/ 1f0 (- near far)))
-         (ar (/ width height)))
-    (m4:make (/ d ar)  0f0  0f0                     0f0
-             0f0       d    0f0                     0f0
-             0f0       0f0  (* (+ near far) recip)  (* 2 near far recip)
-             0f0       0f0  -1f0                    0f0)))
+  (let* ((aspect (/ width height))
+         (tan-half-fov (tan (/ fov 2f0)))
+         (result (m4:0!)))
+    (setf (m4:melm result 0 0) (/ 1f0 (* aspect tan-half-fov))
+          (m4:melm result 1 1) (/ 1f0 tan-half-fov)
+          (m4:melm result 2 3) -1f0
+          ;; (m4:melm result 2 2) (/ far (- near far))
+          ;; (m4:melm result 3 2) (- (/ (* far near) (- far near)))
+          (m4:melm result 2 2) (- (/ (+ far near) (- far near)))
+          (m4:melm result 3 2) (- (/ (* 2f0 far near) (- far near))))
+    result))
 
-;; Another reference implementation below is based on GLM
-;; (defn perspective-radian-fov ((width single-float) (height single-float)
-;;                                (near single-float) (far single-float)
-;;                                (fov single-float))
-;;     mat4
-;;   (declare (optimize (speed 3) (safety 1) (debug 1)))
-;;   (let* ((tan-half-fov (tan (* fov 0.5f0)))
-;;          (ar (/ width height))
-;;          (result (m4:0!)))
-;;     (setf (m4:melm result 0 0) (/ 1f0 (* ar tan-half-fov))
-;;           (m4:melm result 1 1) (/ 1f0 tan-half-fov)
-;;           (m4:melm result 2 3) -1f0
-;;           (m4:melm result 2 2) (- (/ (+ far near) (- far near)))
-;;           (m4:melm result 3 2) (- (/ (* 2 far near) (- far near))))
-;;     result))
 
 (defn perspective ((width single-float) (height single-float)
                    (near single-float) (far single-float)
