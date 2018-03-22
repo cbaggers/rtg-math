6d4d768843e0205a5c008436a4505dca48205e2d
cbaggers <chris.bagley@gmail.com>
2018-03-22 22:32:37 +0100

    had forgotten a few variants of m!

--- vari/matrices/base-matrices.lisp
+++ vari/matrices/base-matrices.lisp
@@ -18,3 +18,12 @@
  (:float :float :float :float :float :float :float :float
          :float :float :float :float :float :float :float :float)
  :mat4 :pure t)
+
+(varjo:v-def-glsl-template-fun
+ m! (a b) "mat2(~a,~a)" (:vec2 :vec2) :mat2 :pure t)
+
+(varjo:v-def-glsl-template-fun
+ m! (a b c) "mat3(~a,~a,~a)" (:vec3 :vec3 :vec3) :mat3 :pure t)
+
+(varjo:v-def-glsl-template-fun
+ m! (a b c d) "mat4(~a,~a,~a,~a)" (:vec4 :vec4 :vec4 :vec4) :mat4 :pure t)
