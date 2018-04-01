49c5d215a11358b1c7eea22c2357f34689555881
baggers <baggers@cave>
2018-04-01 02:18:44 +0200

    move docs for mat2 (consing) will carry on tomorrow

--- matrices/matrix2/consing.lisp
+++ matrices/matrix2/consing.lisp
@@ -1,4 +1,4 @@
-(in-package :rtg-math.matrix2)
+ (in-package :rtg-math.matrix2)
 
 ;; All matrices are stored in column-major format, but when you
 ;; write them (like in m!) then you write them in row-major format.
@@ -6,7 +6,6 @@
 ;;----------------------------------------------------------------
 
 (defn-inline 0! () mat2
-  "Return a 2x2 zero matrix"
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (make-array 4 :element-type `single-float :initial-element 0.0))
 
@@ -14,7 +13,6 @@
 
 (defn make ((a single-float) (b single-float)
             (c single-float) (d single-float)) mat2
-  "Make a 2x2 matrix. Data must be provided in row major order"
   (declare (optimize (speed 3) (safety 1) (debug 1))
            (inline 0!))
   (m2-n:set-components a b c d (0!)))
@@ -33,7 +31,6 @@
 ;;----------------------------------------------------------------
 
 (defn identity () mat2
-  "Return a 2x2 identity matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (make
    1.0 0.0
@@ -42,8 +39,6 @@
 ;;----------------------------------------------------------------
 
 (defn from-rows ((row-1 vec2) (row-2 vec2)) mat2
-  "Make a 2x2 matrix using the data in the 2 vector2s provided
-   to populate the rows"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (make (x row-1) (y row-1)
         (x row-2) (y row-2)))
@@ -51,7 +46,6 @@
 ;;----------------------------------------------------------------
 
 (defn get-rows ((mat-a mat2)) list
-  "Return the rows of the matrix a 2 vector2s"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (list (v2:make (melm mat-a 0 0)
                  (melm mat-a 0 1))
@@ -61,7 +55,6 @@
 ;;----------------------------------------------------------------
 
 (defn get-row ((mat-a mat2) (row-num (integer 0 1))) vec2
-  "Return the specified row of the matrix a vector2"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (v2:make (melm mat-a row-num 0)
            (melm mat-a row-num 1)))
@@ -69,8 +62,6 @@
 ;;----------------------------------------------------------------
 
 (defn from-columns ((col-1 vec2) (col-2 vec2)) mat2
-  "Make a 2x2 matrix using the data in the 2 vector2s provided
-   to populate the columns"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (make (x col-1) (x col-2)
         (y col-1) (y col-2)))
@@ -78,7 +69,6 @@
 ;;----------------------------------------------------------------
 
 (defn get-columns ((mat-a mat2)) list
-  "Return the columns of the matrix as 2 vector2s"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (list (v! (melm mat-a 0 0)
             (melm mat-a 1 0))
@@ -88,7 +78,6 @@
 ;;----------------------------------------------------------------
 
 (defn get-column ((mat-a mat2) (col-num (integer 0 1))) vec2
-  "Return the specified column of the matrix a vector2"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (v2:make (melm mat-a 0 col-num)
            (melm mat-a 1 col-num)))
@@ -96,9 +85,6 @@
 ;;----------------------------------------------------------------
 
 (defn 0p ((mat-a mat2)) boolean
-  "Returns 't' if this is a zero matrix (as contents of the
-   matrix are floats the values have an error bound as defined
-   in base-maths"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (and (cl:= 0f0 (aref mat-a 0))
        (cl:= 0f0 (aref mat-a 1))
@@ -108,9 +94,6 @@
 ;;----------------------------------------------------------------
 
 (defn identityp ((mat-a mat2)) boolean
-  "Returns 't' if this is an identity matrix (as contents of the
-   matrix are floats the values have an error bound as defined
-   in base-maths"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (and (cl:= 1f0 (melm mat-a 0 0)) (cl:= 0f0 (melm mat-a 0 1))
        (cl:= 0f0 (melm mat-a 1 0)) (cl:= 1f0 (melm mat-a 1 1))))
@@ -118,8 +101,6 @@
 ;;----------------------------------------------------------------
 
 (defn = ((mat-a mat2) (mat-b mat2)) boolean
-  "Returns t if all elements of both matrices provided are
-   equal"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (and (cl:= (aref mat-a 0) (aref mat-b 0))
        (cl:= (aref mat-a 1) (aref mat-b 1))
@@ -129,7 +110,6 @@
 ;;----------------------------------------------------------------
 
 (defn transpose ((mat-a mat2)) mat2
-  "Returns the transpose of the provided matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1))
            (inline copy-mat2))
   (m2-n:transpose (copy-mat2 mat-a)))
@@ -137,7 +117,6 @@
 ;;----------------------------------------------------------------
 
 (defn adjoint ((mat-a mat2)) mat2
-  "Returns the adjoint of the provided matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1))
            (inline copy-mat2))
   (m2-n:adjoint (copy-mat2 mat-a)))
@@ -145,14 +124,12 @@
 ;;----------------------------------------------------------------
 
 (defn trace ((mat-a mat2)) single-float
-  "Returns the trace of the matrix (That is the diagonal values)"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (cl:+ (melm mat-a 0 0) (melm mat-a 1 1)))
 
 ;;----------------------------------------------------------------
 
 (defn rotation-from-euler ((angle single-float)) mat2
-  "Returns the transpose of the provided matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1))
            (inline 0!))
   (m2-n:set-rotation-from-euler (0!) angle))
@@ -160,7 +137,6 @@
 ;;----------------------------------------------------------------
 
 (defn scale ((scale-vec2 vec2)) mat2
-  "Returns a matrix which will scale by the amounts specified"
   (declare (optimize (speed 3) (safety 1) (debug 1))
            (inline 0!))
   (m2-n:set-from-scale (0!) scale-vec2))
@@ -168,8 +144,6 @@
 ;;----------------------------------------------------------------
 
 (defn + ((mat-a mat2) (mat-b mat2)) mat2
-  "Adds the 2 matrices component wise and returns the result as
-   a new matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (make (cl:+ (aref mat-a 0) (aref mat-b 0))
         (cl:+ (aref mat-a 1) (aref mat-b 1))
@@ -179,8 +153,6 @@
 ;;----------------------------------------------------------------
 
 (defn - ((mat-a mat2) (mat-b mat2)) mat2
-  "Subtracts the 2 matrices component wise and returns the result
-   as a new matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (make (cl:- (aref mat-a 0) (aref mat-b 0))
         (cl:- (aref mat-a 1) (aref mat-b 1))
@@ -190,7 +162,6 @@
 ;;----------------------------------------------------------------
 
 (defn negate ((mat-a mat2)) mat2
-  "Negates the components of the matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1))
            (inline copy-mat2))
   (m2-n:negate (copy-mat2 mat-a)))
@@ -198,8 +169,6 @@
 ;;----------------------------------------------------------------
 
 (defn *v ((mat-a mat2) (vec-a vec2)) vec2
-  "Multiplies the vector2 by the matrix and returns the result
-   as a new vector2"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (m2-n:*v mat-a (v2:copy-vec2 vec-a)))
 
@@ -217,8 +186,6 @@
 ;; ae+bc  af+bi
 ;; ce+dc  cf+di
 (defn %* ((mat-a mat2) (mat-b mat2)) mat2
-  "Multiplies 2 matrices and returns the result as a new
-   matrix"
   (declare (optimize (speed 3) (safety 1) (debug 1)))
   (make (cl:+ (cl:* (melm mat-a 0 0) (melm mat-b 0 0))
               (cl:* (melm mat-a 0 1) (melm mat-b 1 0)))
@@ -246,8 +213,6 @@
 ;;----------------------------------------------------------------
 
 (defn *s ((mat-a mat2) (scalar single-float)) mat2
-  "Multiplies the components of the matrix by the scalar
-   provided"
   (declare (optimize (speed 3) (safety 1) (debug 1))
            (inline copy-mat2))
   (m2-n:*s (copy-mat2 mat-a) scalar))
--- matrices/matrix2/docs-consing.lisp
+++ matrices/matrix2/docs-consing.lisp
@@ -15,4 +15,91 @@ Returns a fresh `mat2` which is m2:= to the orignal matrix
   (defun mrow*vec2
       "
 {TODO}
-"))
+")
+  (DEFUN 0!
+      "
+Return a `mat2` where all components are zero")
+  (DEFUN MAKE
+      "
+Make a `mat2`. Data must be provided in row major order")
+  (DEFUN IDENTITY
+      "
+Return a `mat2` identity matrix")
+  (DEFUN FROM-ROWS
+      "
+Make a `mat2` using the data in the 2 `vec2`s to populate the rows")
+  (DEFUN GET-ROWS
+      "
+Return the rows of the matrix as 2 `vec2`s")
+  (DEFUN GET-ROW
+      "
+Return the specified row of the matrix as a `vec2`")
+  (DEFUN FROM-COLUMNS
+
+      "
+Make a `mat2` using the data in the 2 `vec2`s to populate the columns")
+  (DEFUN GET-COLUMNS
+      "
+Return the columns of the matrix as 2 `vec2`s")
+  (DEFUN GET-COLUMN
+      "
+Return the specified column of the matrix as a `vec2`")
+  (DEFUN 0P
+      "
+Returns T if this is a zero matrix
+
+As contents of the matrix are floats the values have an error bound as defined
+in base-maths
+
+Note: Old, float epsilons are tricky, I have a ticket open to review this")
+  (DEFUN IDENTITYP
+      "
+Returns T if this is an identity matrix
+
+As contents of the matrix are floats the values have an error bound as defined
+in base-maths
+
+Note: Old, float epsilons are tricky, I have a ticket open to review this")
+  (DEFUN =
+      "
+Returns T if all elements of both matrices provided are equal")
+  (DEFUN TRANSPOSE
+      "
+Returns the transpose of the provided matrix")
+  (DEFUN ADJOINT
+      "
+Returns the adjoint of the provided matrix")
+  (DEFUN TRACE
+      "
+Returns the trace of the matrix (That is the diagonal values)")
+  (DEFUN ROTATION-FROM-EULER
+      "
+Creates a rotation `mat2` which represents a anticlockwise rotation.
+
+The angle is specified in radians")
+  (DEFUN SCALE
+      "
+Returns a matrix which will scale by the amounts specified")
+  (DEFUN +
+      "
+Adds the 2 matrices component wise and returns the result as
+a new `mat2`")
+  (DEFUN -
+      "
+Subtracts the 2 matrices component wise and returns the result
+as a new `mat2`")
+  (DEFUN NEGATE
+      "
+Negates the components of the `mat2`")
+  (DEFUN *V
+      "
+Multiplies the `vec2` by the `mat2` and returns the result
+as a new `vec2`")
+  (DEFUN %*
+      "
+Multiplies 2 matrices and returns the result as a new
+`mat2`")
+  (DEFUN *S
+      "
+Multiplies the components of the `mat2` by the scalar
+provided"))
